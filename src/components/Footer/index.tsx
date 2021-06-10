4c8b13cd518e9f271f8245f4190408e9829753a0
LiangJunChan <liangjunchen@m.scnu.edu.cn>
2021-06-10 14:57:31 +0800

    fe
    博客前端框架

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,37 @@
+import { useIntl } from 'umi';
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-layout';
+
+export default () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'ChenLiangjun',
+    defaultMessage: 'ChenLiangjun',
+  });
+
+  return (
+    <DefaultFooter
+      copyright={`2021 ${defaultMessage}`}
+      links={[
+        {
+          key: 'Ant Design Pro',
+          title: 'WeiBo',
+          href: 'https://weibo.com/2246702743/profile?topnav=1&wvr=6',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/LiangJunChan',
+          blankTarget: true,
+        },
+        // {
+        //   key: 'Ant Design',
+        //   title: 'Ant Design',
+        //   href: 'https://ant.design',
+        //   blankTarget: true,
+        // },
+      ]}
+    />
+  );
+};
